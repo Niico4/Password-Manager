eab765cc9172838dd03da60e0d656301dfa1c6f4
“Niico4” <nicogarzon131@gmail.com>
2024-10-20 04:02:18 -0500

    feat(deploy) ajustar next.config quitando datos innecesarios

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-// import { resolve, dirname } from 'path';
-// import { fileURLToPath } from 'url';
-
-// Obtén la ruta del directorio actual
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -17,14 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  // webpack: (config) => {
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     '@': resolve(__dirname, './'),
-  //   };
-
-  //   return config;
-  // },
 };
 
 export default nextConfig;
